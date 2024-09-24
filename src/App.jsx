0e3bd3edba6afd7ea7a7690359543b800282524d
ThomasLlopiz <toomillopiz@gmail.com>
2024-09-24 16:41:15 -0300

    cambio necesario para recargar

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import React from "react";
 import { AppRouter } from "./Router/AppRouter";
 import { Nav } from "./components/Nav";
 import { Footer } from "./components/Footer";
+import { BrowserRouter as Router } from "react-router-dom"; 
 
 const App = () => {
   return (
     <>
-      <div className="hero w-full h-full flex justify-center align-center min-h-screen"></div>
-      <Nav></Nav>
-      <div className="min-h-screen color-white flex flex-col justify-center items-start bg-black font-black not-italic">
-        <AppRouter></AppRouter>
-        <hr className="relative w-2/3 mx-auto top-0 h-[2px] min-w-[18rem] border-t-0 bg-transparent bg-gradient-to-r from-transparent via-white to-transparent bg-center md:my-9" />
-        <Footer />
-      </div>
+      <Router>
+        <div className="hero w-full h-full flex justify-center align-center min-h-screen"></div>
+        <Nav></Nav>
+        <div className="min-h-screen color-white flex flex-col justify-center items-start bg-black font-black not-italic">
+          <AppRouter></AppRouter>
+          <hr className="relative w-2/3 mx-auto top-0 h-[2px] min-w-[18rem] border-t-0 bg-transparent bg-gradient-to-r from-transparent via-white to-transparent bg-center md:my-9" />
+          <Footer />
+        </div>
+      </Router>
     </>
   );
 };
