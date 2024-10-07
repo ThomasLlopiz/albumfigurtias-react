e195871c2d23382d9ee27280fd708df39da0206a
ThomasLlopiz <toomillopiz@gmail.com>
2024-10-07 12:31:02 -0300

    borrando comentarios

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,14 +5,12 @@ export const Banner = () => {
   return (
     <section className="mt-40 relative md:mt-80">
       <div className="flex gap-20 justify-center items-center">
-        {/* <!-- Imagenes para desktop --> */}
         <div className="w-2/5 text-white hidden md:block">
           <img src="./imagenes/general/album.png" alt="Album Cover" />
         </div>
         <div className="w-2/5 text-white hidden md:block">
           <img src="./imagenes/general/heroimage.png" alt="Hero Image" />
         </div>
-        {/* <!-- Imagen para mobile --> */}
         <div className="w-5/6 text-white block md:hidden">
           <img src="./imagenes/general/albummobile.png" alt="Album Mobile" />
         </div>
