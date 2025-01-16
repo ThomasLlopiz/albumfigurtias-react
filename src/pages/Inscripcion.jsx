e8b2f5f8379370fcb000c1105e288975da791a0b
ThomasLlopiz <toomillopiz@gmail.com>
2025-01-16 10:57:38 -0300

    inscripcion a ssd

--- src/pages/Inscripcion.jsx
+++ src/pages/Inscripcion.jsx
@@ -1,90 +1,194 @@
+import { useState } from "react";
+
 export const Inscripcion = () => {
+  const [nombre, setNombre] = useState("");
+  const [apellido, setApellido] = useState("");
+  const [edad, setEdad] = useState("");
+  const [direccion, setDireccion] = useState("");
+  const [responsable, setResponsable] = useState("");
+  const [telefono, setTelefono] = useState("");
+  const [FechaNacimiento, setFechaNacimiento] = useState("");
+  const [deporte, setDeporte] = useState("");
+
+  // Función que se ejecuta cuando se cambia la edad
+  const handleEdadChange = (event) => {
+    setEdad(event.target.value);
+  };
+
   return (
-    <div className="mt-32 flex flex-col items-center bg-black">
-      <h1>Inscripcion</h1>
-      <form>
-        <div className="flex items-center justify-between gap-3 text-center">
-          <label htmlFor="nombre">Nombre</label>
-          <input
-            className="w-32"
-            type="text"
-            id="nombre"
-            name="nombre"
-            placeholder="Nombre"
-          />
+    <div className="mt-20 mb-20 flex flex-col items-center text-black">
+      <h1 className="text-xl font-semibold">INSCRIPCION</h1>
+      <div className="flex justify-around gap-20 w-1/2">
+        <form className="mx-auto mt-10 w-1/2">
+          <div className="grid md:grid-cols-2 md:gap-6">
+            <div className="relative z-0 w-full mb-5 group">
+              <input
+                type="text"
+                name="floating_first_name"
+                id="floating_first_name"
+                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer"
+                placeholder=" "
+                required
+              />
+              <label
+                htmlFor="floating_first_name"
+                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                Nombre
+              </label>
+            </div>
+            <div className="relative z-0 w-full mb-5 group">
+              <input
+                type="text"
+                name="floating_last_name"
+                id="floating_last_name"
+                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer"
+                placeholder=" "
+                required
+              />
+              <label
+                htmlFor="floating_last_name"
+                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                Apellido
+              </label>
+            </div>
+          </div>
+
+          <div className="grid md:grid-cols-2 md:gap-6">
+            <div className="relative z-0 w-full mb-5 group">
+              <input
+                type="number"
+                name="floating_edad"
+                id="floating_edad"
+                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer"
+                placeholder=" "
+                value={edad}
+                onChange={handleEdadChange} // Actualiza la edad
+                required
+              />
+              <label
+                htmlFor="floating_edad"
+                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                Edad
+              </label>
+            </div>
+            <div className="relative z-0 w-full mb-5 group">
+              <input
+                type="text"
+                name="direccion"
+                id="floating_direccionn"
+                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer"
+                placeholder=" "
+                required
+              />
+              <label
+                htmlFor="floating_direccion"
+                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                Direccion
+              </label>
+            </div>
+          </div>
+
+          {/* Solo mostrar el campo de "Responsable del menor" si la edad ha sido ingresada y es menor a 18 */}
+          {edad !== "" && edad < 18 && (
+            <div className="relative z-0 w-full mb-5 group">
+              <input
+                type="text"
+                name="floating_responsable"
+                id="floating_responsable"
+                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer"
+                placeholder=" "
+                value={responsable}
+                onChange={(e) => setResponsable(e.target.value)} // Actualiza el responsable
+                required
+              />
+              <label
+                htmlFor="floating_responsable"
+                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                Responsable del menor
+              </label>
+            </div>
+          )}
+
+          <div className="relative z-0 w-full mb-5 group">
+            <input
+              type="tel"
+              pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+              name="floating_phone"
+              id="floating_phone"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer"
+              placeholder=" "
+              required
+            />
+            <label
+              htmlFor="floating_phone"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Telefono (3564 - 598173)
+            </label>
+          </div>
+
+          <div className="grid md:grid-cols-2 md:gap-6">
+            <div className="relative z-0 w-full mb-5 group">
+              <input
+                type="date"
+                name="floating_birthdate"
+                id="floating_birthdate"
+                className="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer"
+                required
+              />
+              <label
+                htmlFor="floating_birthdate"
+                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                Fecha de nacimiento
+              </label>
+            </div>
+
+            <div className="relative z-0 w-full mb-5 group">
+              <select
+                name="floating_sport"
+                id="floating_sport"
+                className="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer"
+                required
+              >
+                <option value="" disabled selected hidden>
+                  Seleccione un deporte
+                </option>
+                <option value="basquet">Basquet</option>
+                <option value="voley">Voley</option>
+                <option value="futbol">Fútbol</option>
+                <option value="tenis">Tenis</option>
+              </select>
+              <label
+                htmlFor="floating_sport"
+                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              ></label>
+            </div>
+          </div>
+
+          <button
+            type="submit"
+            className="w-full text-white bg-green-900 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm sm:w-full px-5 py-2.5 text-center dark:bg-green-800 dark:hover:bg-green-500 dark:focus:ring-green-300"
+          >
+            ENVIAR
+          </button>
+        </form>
+        <div className="w-1/2 mt-20 mx-auto flex flex-col justify-between">
+          <p className="text-lg">
+            La inscripcion a cualquier deporte estara efectivizada luego de
+            entregar la ficha, complearla, firmarla y entregarla en secretaría o
+            al profesor correspondiente.
+          </p>
+          <button className="w-full bg-green-800 p-2 rounded-lg text-white hover:bg-green-500">
+            DESCARGAR FICHA
+          </button>
         </div>
-        <div className="flex items-center justify-between gap-3 text-center">
-          <label htmlFor="apellido">Apellido</label>
-          <input
-            className="w-32"
-            type="text"
-            id="apellido"
-            name="apellido"
-            placeholder="Apellido"
-          />
-        </div>
-        <div className="flex items-center justify-between gap-3 text-center">
-          <label htmlFor="email">Email</label>
-          <input
-            className="w-32"
-            type="email"
-            id="email"
-            name="email"
-            placeholder="Email"
-          />
-        </div>
-        <div className="flex items-center justify-between gap-3 text-center">
-          <label htmlFor="telefono">Telefono</label>
-          <input
-            className="w-32"
-            type="tel"
-            id="telefono"
-            name="telefono"
-            placeholder="Telefono"
-          />
-        </div>
-        <div className="flex items-center justify-between gap-3 text-center">
-          <label htmlFor="edad">Edad</label>
-          <input
-            className="w-32"
-            type="number"
-            id="edad"
-            name="edad"
-            placeholder="Edad"
-          />
-        </div>
-        <div className="flex items-center justify-between gap-3 text-center">
-          <label htmlFor="genero">Genero</label>
-          <input
-            className="w-32"
-            type="text"
-            id="genero"
-            name="genero"
-            placeholder="Genero"
-          />
-        </div>
-        <div className="flex items-center justify-between gap-3 text-center">
-          <label htmlFor="direccion">Direccion</label>
-          <input
-            className="w-32"
-            type="text"
-            id="direccion"
-            name="direccion"
-            placeholder="Direccion"
-          />
-        </div>
-        <div className="flex items-center justify-between gap-3 text-center">
-          <label htmlFor="deporte">Deporte</label>
-          <input
-            className="w-32"
-            type="text"
-            id="deporte"
-            name="deporte"
-            placeholder="Deporte"
-          />
-        </div>
-      </form>
-      <button type="submit">Enviar</button>
+      </div>
     </div>
   );
 };
