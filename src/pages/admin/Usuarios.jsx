b6b92513bc948fb3b49629724b293c3aaf3e5f69
ThomasLlopiz <toomillopiz@gmail.com>
2025-02-18 08:51:54 -0300

    aceptando rol en minuscula

--- src/pages/admin/Usuarios.jsx
+++ src/pages/admin/Usuarios.jsx
@@ -1,13 +1,13 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 export const Usuarios = () => {
-    const [usuario, setUsuario] = useState('');
-    const [contrasena, setContrasena] = useState('');
-    const [rol, setRol] = useState('');
+    const [usuario, setUsuario] = useState("");
+    const [contrasena, setContrasena] = useState("");
+    const [rol, setRol] = useState("");
     const [usuarios, setUsuarios] = useState([]);
 
     const fetchUsuarios = async () => {
-        const response = await fetch('http://localhost:8000/api/usuarios');
+        const response = await fetch("http://localhost:8000/api/usuarios");
         const data = await response.json();
 
         if (response.ok) {
@@ -24,10 +24,10 @@ export const Usuarios = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        const response = await fetch('http://localhost:8000/api/usuarios', {
-            method: 'POST',
+        const response = await fetch("http://localhost:8000/api/usuarios", {
+            method: "POST",
             headers: {
-                'Content-Type': 'application/json',
+                "Content-Type": "application/json",
             },
             body: JSON.stringify({
                 usuario,
@@ -42,76 +42,95 @@ export const Usuarios = () => {
         try {
             const data = JSON.parse(text);
             if (response.ok) {
-                alert('Usuario creado con éxito');
+                alert("Usuario creado con éxito");
                 fetchUsuarios();
-                setUsuario('');
-                setContrasena('');
-                setRol('');
+                setUsuario("");
+                setContrasena("");
+                setRol("");
             } else {
                 alert(`Error: ${data.message}`);
             }
         } catch (error) {
-            console.error('Error al parsear JSON:', error);
-            alert('Error al crear el usuario');
+            console.error("Error al parsear JSON:", error);
+            alert("Error al crear el usuario");
         }
     };
 
-
-
     return (
-        <div className="mt-40 mb-32 text-black flex flex-wrap md:gap-12 mx-auto justify-center items-center">
-            <form onSubmit={handleSubmit} className='flex flex-col gap-4 w-4/6 md:w-1/6'>
-                <input
-                    className='w-full border-2 py-2 rounded-lg font-semibold border-black pl-2'
-                    type="text"
-                    placeholder="Usuario"
-                    value={usuario}
-                    onChange={(e) => setUsuario(e.target.value)}
-                />
-                <input
-                    className='w-full border-2 py-2 rounded-lg font-semibold border-black pl-2'
-                    type="password"
-                    placeholder="Contraseña"
-                    value={contrasena}
-                    onChange={(e) => setContrasena(e.target.value)}
-                />
-                <select
-                    className="w-full border-2 py-2 rounded-lg font-semibold border-black pl-2"
-                    value={rol}
-                    onChange={(e) => setRol(e.target.value)}
+        <div className="mt-40 mb-32 text-black flex flex-col items-center justify-center gap-3">
+            <div className="flex md:gap-12 mx-auto justify-center items-center">
+                <form
+                    onSubmit={handleSubmit}
+                    className="flex flex-col gap-4 w-4/6 md:w-4/6"
                 >
-                    <option value="" disabled>Selecciona un rol</option>
-                    <option value="Profesor">Profesor</option>
-                    <option value="Admin">Admin</option>
-                </select>
+                    <input
+                        className="w-full border-2 py-2 rounded-lg font-semibold border-black pl-2"
+                        type="text"
+                        placeholder="Usuario"
+                        value={usuario}
+                        onChange={(e) => setUsuario(e.target.value)}
+                    />
+                    <input
+                        className="w-full border-2 py-2 rounded-lg font-semibold border-black pl-2"
+                        type="password"
+                        placeholder="Contraseña"
+                        value={contrasena}
+                        onChange={(e) => setContrasena(e.target.value)}
+                    />
+                    <select
+                        className="w-full border-2 py-2 rounded-lg font-semibold border-black pl-2"
+                        value={rol}
+                        onChange={(e) => setRol(e.target.value)}
+                    >
+                        <option value="" disabled>
+                            Selecciona un rol
+                        </option>
+                        <option value="profesor">profesor</option>
+                        <option value="admin">admin</option>
+                    </select>
 
-                <button
-                    className="bg-green-600 hover:bg-green-800 text-white py-2 px-8 rounded-lg font-bold transition duration-200"
-                    type="submit">Crear Usuario</button>
-            </form>
+                    <button
+                        className="bg-green-600 hover:bg-green-800 text-white py-2 px-8 rounded-lg font-bold transition duration-200"
+                        type="submit"
+                    >
+                        Crear Usuario
+                    </button>
+                </form>
 
-            <div className="text-left">
-                {usuarios.length > 0 ? (
-                    <table className="mt-4 border-collapse w-full text-left">
-                        <thead>
-                            <tr>
-                                <th className="border-b-2 py-2 text-left px-5">Nombre de Usuario</th>
-                                <th className="border-b-2 py-2 text-left px-5">Rol</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {usuarios.map((user, index) => (
-                                <tr key={index}>
-                                    <td className="border-b py-2 text-left px-5">{user.usuario}</td>
-                                    <td className="border-b py-2 text-left px-5">{user.rol}</td>
+                <div className="text-left">
+                    {usuarios.length > 0 ? (
+                        <table className="mt-4 border-collapse w-72 text-left">
+                            <thead>
+                                <tr>
+                                    <th className="border-b-2 py-2 text-left px-5">
+                                        Usuario
+                                    </th>
+                                    <th className="border-b-2 py-2 text-left px-5">Rol</th>
                                 </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                ) : (
-                    <p>No hay usuarios registrados.</p>
-                )}
+                            </thead>
+                            <tbody>
+                                {usuarios.map((user, index) => (
+                                    <tr key={index}>
+                                        <td className="border-b py-2 text-left px-5">
+                                            {user.usuario}
+                                        </td>
+                                        <td className="border-b py-2 text-left px-5">{user.rol}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    ) : (
+                        <p>No hay usuarios registrados.</p>
+                    )}
+                </div>
             </div>
+
+            <button
+                onClick={() => navigate("/admin")}
+                className="bg-green-600 hover:bg-green-800 text-white py-2 px-6 rounded-lg font-bold transition duration-200"
+            >
+                Volver
+            </button>
         </div>
     );
 };
