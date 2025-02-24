e66618ad12e448477f3619a3578dc847f9f2ed01
ThomasLlopiz <toomillopiz@gmail.com>
2025-02-24 17:03:25 -0300

    Youtube

---
+++ src/components/Youtube.jsx
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from 'react';
+
+const API_KEY = 'AIzaSyCurKMfpNTO2cDqPnUBzwILHu9i3ziVEb0';
+const CHANNEL_ID = 'UCORkF7eXlKSfcCN4B4c869g';
+const API_URL = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}&order=date&part=snippet&type=video&maxResults=12`;
+
+export const Youtube = () => {
+  const [videos, setVideos] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  // Cargar videos al montar el componente
+  useEffect(() => {
+    const loadVideos = async () => {
+      try {
+        const response = await fetch(API_URL);
+        const data = await response.json();
+        setVideos(data.items);
+      } catch (error) {
+        console.error('Error al cargar los videos', error);
+      }
+    };
+
+    loadVideos();
+  }, []);
+
+  // Configurar el carrusel
+  useEffect(() => {
+    if (videos.length > 0) {
+      const interval = setInterval(() => {
+        setCurrentIndex((prevIndex) => {
+          const totalVideos = videos.length;
+          return prevIndex < totalVideos - 3 ? prevIndex + 1 : 0;
+        });
+      }, 2000);
+
+      return () => clearInterval(interval); // Limpieza al desmontar
+    }
+  }, [videos]);
+
+  return (
+    <div id="youtube-videos" className="relative overflow-hidden pb-20 max-w-7xl mx-auto text-black">
+      <div className="flex items-center justify-start p-2 text-gray-800 rounded-lg">
+        <i className="fa-brands fa-youtube text-red-600 text-xl mr-2"></i>
+        <a
+          target="_blank"
+          href="https://www.youtube.com/@SociedadSportivaDevoto"
+          className="text-md font-semibold"
+          rel="noopener noreferrer"
+        >
+          Seguinos en YouTube
+        </a>
+      </div>
+
+      <div
+        id="video-carousel"
+        className="flex transition-transform duration-500 ease-in-out"
+        style={{ transform: `translateX(-${currentIndex * 33.33}%)` }}
+      >
+        {videos.map((item) => {
+          const videoId = item.id.videoId;
+          const videoTitle = item.snippet.title;
+          const videoThumbnail = item.snippet.thumbnails.medium.url;
+          const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
+
+          return (
+            <div
+              key={videoId}
+              className="video-card flex-shrink-0 w-1/3 px-2"
+            >
+              <a href={videoUrl} target="_blank" rel="noopener noreferrer">
+                <img
+                  src={videoThumbnail}
+                  alt={videoTitle}
+                  className="w-full h-auto rounded-md"
+                />
+                <h3 className="text-sm mt-2">{videoTitle}</h3>
+              </a>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
