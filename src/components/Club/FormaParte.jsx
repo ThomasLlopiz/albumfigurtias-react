4de32f5cb9e802685183590685ad11856f10c965
ThomasLlopiz <toomillopiz@gmail.com>
2025-02-25 11:05:27 -0300

    forma parte

--- src/components/Club/FormaParte.jsx
+++ src/components/Club/FormaParte.jsx
@@ -12,7 +12,7 @@ export const FormaParte = () => {
             <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-between sm:px-32 z-10">
                 <div className="text-white text-center sm:mx-2 mx-auto">
                     <h2 className="text-4xl font-bold mb-4">FORMA PARTE DEL CLUB</h2>
-                    <a href="./trabajaconnosotros.php" aria-label="Postuarme"
+                    <a href="trabajaconnosotros" aria-label="Postuarme"
                         className="text-xl border border-white rounded-lg p-2 mt-4 uppercase mx-auto">Subir mi Curriculum
                         Vitae</a>
                 </div>
