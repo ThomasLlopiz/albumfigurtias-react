fa8806b31b7d9d7b611923060e0286b0e77e36cc
ThomasLlopiz <toomillopiz@gmail.com>
2025-02-26 16:37:29 -0300

    Voley

---
+++ src/pages/club/Voley.jsx
@@ -0,0 +1,47 @@
+export const Voley = () => {
+    return (
+        <section className="py-4 relative my-20">
+            <div className="w-full max-w-7xl px-4 md:px-5 lg:px-5 mx-auto">
+                <div className="w-full justify-start items-center gap-12">
+                    <div className="w-full flex-col justify-center lg:items-start items-center gap-10 inline-flex">
+                        <div className="w-full flex-col justify-center items-start gap-8 flex">
+                            <div className="w-full flex-col justify-start lg:items-start items-center gap-3 flex">
+                                <h2 className="text-gray-900 text-4xl font-bold font-manrope leading-normal lg:text-start text-left">
+                                    Vóley
+                                </h2>
+                                <p className="text-gray-500 text-base font-normal leading-relaxed lg:text-start text-left">
+                                    El vóley de Sociedad Sportiva Devoto es una de nuestras actividades más convocantes y
+                                    una verdadera pasión que une a nuestra comunidad. Formamos parte de la Asociación de
+                                    Vóley de San Francisco, compitiendo en su liga con equipos en todas las categorías,
+                                    desde las más pequeñas hasta la Primera División.
+                                    <br /><br />
+                                    Contamos con un amplio abanico de categorías, que incluyen: Pre Mini, Mini, Mini
+                                    Avanzado, Sub 12, Sub 13, Sub 14, Sub 16, Sub 18, Primera División Femenina y Maxi Vóley
+                                    Femenino, desde el 2025 se cuenta también con vóley masculino en categorías Sub 16, Sub
+                                    18 y Primera.
+                                    <br /><br />
+                                    Uno de los torneos más destacados que organizamos es el Torneo Bianca Cugno, un evento
+                                    que convoca a numerosos equipos y jugadores de la región. Este torneo lleva el nombre de
+                                    Bianca Cugno, nuestra gran referente y orgullo. Bianca, una destacada jugadora de la
+                                    Selección Nacional de Vóley Femenino, ha sido un ejemplo de dedicación y éxito,
+                                    brindándonos innumerables satisfacciones con sus logros y su contribución al deporte.
+                                    <br /><br />
+                                    Además, nuestros equipos participan regularmente en diversos torneos y competencias
+                                    organizados por clubes y entidades deportivas de la región, lo que nos permite
+                                    mantenernos siempre en constante competencia y crecimiento.
+                                    <br /><br />
+                                    En Sociedad Sportiva Devoto, el vóley es mucho más que una disciplina deportiva: es un
+                                    espacio donde el compromiso, la pasión y el trabajo en equipo se combinan para generar
+                                    un ambiente único de camaradería y desarrollo.
+                                    <br /><br />
+                                    Para destacar durante el año se realizan diversos torneos de vóley recreativo,
+                                    convocando tanto a hombres como mujeres, quienes disfrutan del deporte y la amistad.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
