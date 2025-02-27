3b1776cec08c530c95474207d3ab171a5749471b
ThomasLlopiz <toomillopiz@gmail.com>
2025-02-27 09:45:00 -0300

    link a inscribirme

--- src/components/Club/BannerInscribite.jsx
+++ src/components/Club/BannerInscribite.jsx
@@ -1,9 +1,12 @@
 
+import { Link } from "react-router-dom"
 export const BannerInscribite = () => {
     return (
         <div className="max-w-7xl mx-auto px-4 my-10 rounded-md">
-            <img src="./imagenes/club/publicidad1.jpg" alt="Publicidad" className="w-full h-auto rounded-lg hidden xl:block" />
-            <img src="./imagenes/club/publicidad1mobile.jpg" alt="Publicidad" className="w-full h-auto rounded-lg block xl:hidden" />
+            <Link to="inscribirme">
+                <img src="./imagenes/club/publicidad1.jpg" alt="Publicidad" className="w-full h-auto rounded-lg hidden xl:block" />
+                <img src="./imagenes/club/publicidad1mobile.jpg" alt="Publicidad" className="w-full h-auto rounded-lg block xl:hidden" />
+            </Link>
         </div>
     )
 }
