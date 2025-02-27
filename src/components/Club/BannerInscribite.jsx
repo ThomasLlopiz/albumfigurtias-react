cf8683329cab670a868846658f6a0a638a336816
ThomasLlopiz <toomillopiz@gmail.com>
2025-02-27 09:28:41 -0300

    BannerInscribite

---
+++ src/components/Club/BannerInscribite.jsx
@@ -0,0 +1,10 @@
+
+export const BannerInscribite = () => {
+    return (
+        <div className="max-w-7xl mx-auto px-4 my-10 rounded-md">
+            <img src="./imagenes/club/publicidad1.jpg" alt="Publicidad" className="w-full h-auto rounded-lg hidden xl:block" />
+            <img src="./imagenes/club/publicidad1mobile.jpg" alt="Publicidad" className="w-full h-auto rounded-lg block xl:hidden" />
+        </div>
+    )
+}
+
