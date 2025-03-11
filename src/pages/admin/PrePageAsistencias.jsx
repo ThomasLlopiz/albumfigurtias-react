8465ab283d8c0e4e1ccb050fab369749fd3f4c10
ThomasLlopiz <toomillopiz@gmail.com>
2025-03-11 10:01:06 -0300

    estilos a prepageasitencias

--- src/pages/admin/PrePageAsistencias.jsx
+++ src/pages/admin/PrePageAsistencias.jsx
@@ -7,7 +7,7 @@ export const PrePageAsistencias = () => {
     return (
         <div className="my-64 text-white text-center font-semibold text-xl">
             <div className="flex items-center justify-around mb-10 lg:w-3/4 mx-auto">
-                <h1 className="text-2xl font-semibold text-black">Asistencias (No Acumuladas)</h1>
+                <h1 className="text-2xl font-semibold text-black">Asistencias</h1>
                 <button
                     onClick={() => navigate("/admin")}
                     className="bg-green-600 hover:bg-green-800 text-white py-2 px-6 rounded-lg font-bold transition duration-200"
@@ -16,7 +16,7 @@ export const PrePageAsistencias = () => {
                 </button>
             </div>
             <div className=" flex flex-wrap items-center justify-center gap-4">
-                <Link className="bg-green-800 px-4 py-8 rounded-lg" to="/asistencias">Asistencias <p>totales</p></Link>
+                <Link className="bg-green-800 px-4 py-8 rounded-lg" to="/asistencias">Asistencias <p>acumuladas</p></Link>
                 <Link className="bg-green-800 px-4 py-8 rounded-lg" to="/asistenciasNoAcumuladas">Asistencias <p>individuales</p></Link>
             </div>
         </div>
